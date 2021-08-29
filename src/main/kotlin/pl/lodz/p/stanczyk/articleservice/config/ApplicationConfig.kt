61da8d200792156bc350e610495adb24ec462e73
4Kaze <32jacke32@gmail.com>
2021-08-29 18:08:37 +0200

    initial commit

---
+++ src/main/kotlin/pl/lodz/p/stanczyk/articleservice/config/ApplicationConfig.kt
@@ -0,0 +1,28 @@
+package pl.lodz.p.stanczyk.articleservice.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import pl.lodz.p.stanczyk.articleservice.adapter.MongoArticleRepositoryAdapter
+import pl.lodz.p.stanczyk.articleservice.domain.ArticleService
+import pl.lodz.p.stanczyk.articleservice.domain.port.ArticleRepository
+import pl.lodz.p.stanczyk.articleservice.infrastructure.InstantNowSupplier
+import pl.lodz.p.stanczyk.articleservice.infrastructure.InstantNowSupplierImpl
+import pl.lodz.p.stanczyk.articleservice.infrastructure.mongo.MongoArticleRepository
+
+@Configuration
+class ApplicationConfig {
+    @Bean
+    fun articleService(mongoUserRepository: MongoArticleRepository) = ArticleService(
+        articleRepository = articleRepository(mongoUserRepository),
+        instantNowSupplier = instantNowSupplier()
+    )
+
+    @Bean
+    fun articleRepository(mongoUserRepository: MongoArticleRepository): ArticleRepository =
+        MongoArticleRepositoryAdapter(
+            mongoRepository = mongoUserRepository
+        )
+
+    @Bean
+    fun instantNowSupplier(): InstantNowSupplier = InstantNowSupplierImpl()
+}
