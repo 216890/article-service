79cfbc4788ab6c767aa3b57c4c9178daf3e6e477
216890 <%NY532s2aC$Y3D>
2021-11-20 20:16:19 +0100

    remove application tag

--- src/main/kotlin/pl/lodz/p/stanczyk/articleservice/config/ApplicationConfig.kt
+++ src/main/kotlin/pl/lodz/p/stanczyk/articleservice/config/ApplicationConfig.kt
@@ -28,10 +28,4 @@ class ApplicationConfig {
 
     @Bean
     fun instantNowSupplier(): InstantNowSupplier = InstantNowSupplierImpl()
-
-    @Bean
-    fun metricsCommonTags(@Value("spring.application.name") applicationName: String): MeterRegistryCustomizer<MeterRegistry> =
-        MeterRegistryCustomizer { registry: MeterRegistry ->
-            registry.config().commonTags("application", applicationName)
-        }
 }
